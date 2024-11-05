153e7da48a1b72adc4f92a239925950cbf39dd77
eukerolyne <ribeirodias2003@gmail.com>
2024-11-05 10:27:16 -0300

    feat/01: configurando ef e banco de dados

---
+++ Infraestrutura/DB/DbContexto.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
+namespace MinimalApi.Infraestrutura.DB;
+
+public class DbContexto : DbContext
+{
+    public DbContexto(DbContextOptions<DbContexto> options)
+            : base(options)
+    {
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.DB;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEntityFrameworkSqlServer()
+            .AddDbContext<DbContexto>(
+            options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//builder.Services.AddDbContext<DbContexto>(options =>
+//    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
